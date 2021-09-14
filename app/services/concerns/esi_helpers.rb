95d1967f280a5b9567f0ab8daca03f8008073d0b
Bokobo Shahni <shahni@bokobo.space>
2021-09-14 12:55:24 -0400

    refactor: pass authorization instead of character id

--- app/services/concerns/esi_helpers.rb
+++ app/services/concerns/esi_helpers.rb
@@ -7,12 +7,13 @@ module ESIHelpers
   extend ActiveSupport::Concern
 
   def esi
-    @esi ||= ESI::Client.new(user_agent: Rails.application.config.x.esi.user_agent)
+    @esi ||= ESI::Client.new(user_agent: Rails.application.config.x.esi.user_agent,
+                             cache: { store: Rails.cache, logger: Rails.logger,
+                                      instrumenter: ActiveSupport::Notifications })
   end
 
-  def esi_authorize!(character_id)
-    authorization = Authorization.find_by!(character_id: character_id, kind: 'esi')
-    authorization.refresh_esi_token!
+  def esi_authorize!(authorization)
+    authorization.refresh_token!
     esi.authorize(authorization.access_token)
   end
 end
